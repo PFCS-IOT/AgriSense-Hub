1658061005bbd8d224da29a2f2d324da70331290
Livens <dangkhoah77@gmail.com>
2025-12-13 23:28:35 +0700

    [refactor] improve formatting and readability in AuthHandler function

--- Web/src/server/src/Modules/Socket.ts
+++ Web/src/server/src/Modules/Socket.ts
@@ -15,33 +15,41 @@ import { iotHandler } from 'Server/Modules/Mqtt/Handler.js'
  * @param next - The next middleware function
  */
 const AuthHandler = async (socket: Socket, next: (err?: Error) => void) => {
-    const { token = null } = socket.handshake.auth as { token?: string }
+	const { token = null } = socket.handshake.auth as { token?: string }
 
-    // Validate JWT Token
-    if (token) {
-        // Split "Bearer <token>" and extract the token value
-        const [authType, tokenValue] = token.trim().split(' ')
-        if (authType !== 'Bearer' || !tokenValue)
-            return next(new Error('no token'))
+	// Validate JWT Token
+	if (token) {
+		// Split "Bearer <token>" and extract the token value
+		const [authType, tokenValue] = token.trim().split(' ')
+		console.log(
+			'Token: ',
+			token,
+			'\nAfter split: ',
+			authType,
+			' - ',
+			tokenValue
+		)
+		if (authType !== 'Bearer' || !tokenValue)
+			return next(new Error('no token'))
 
-        try {
-            // Verify token and fetch user
-            const payload = jwt.verify(tokenValue, Keys.jwt.secret) as {
-                id: string
-            }
-            const id = payload.id.toString()
-            const user = await User.findById(id)
+		try {
+			// Verify token and fetch user
+			const payload = jwt.verify(tokenValue, Keys.jwt.secret) as {
+				id: string
+			}
+			const id = payload.id.toString()
+			const user = await User.findById(id)
 
-            // Check if user exists
-            if (!user) return next(new Error('No user found'))
-        } catch (err) {
-            return next(new Error('Invalid token', { cause: err }))
-        }
-    } else {
-        return next(new Error('No token provided'))
-    }
+			// Check if user exists
+			if (!user) return next(new Error('No user found'))
+		} catch (err) {
+			return next(new Error('Invalid token', { cause: err }))
+		}
+	} else {
+		return next(new Error('No token provided'))
+	}
 
-    next()
+	next()
 }
 
 /**
@@ -50,21 +58,21 @@ const AuthHandler = async (socket: Socket, next: (err?: Error) => void) => {
  * @param server - The HTTPS server instance
  */
 const socketInit = (server: HttpServer) => {
-    const io = new Server(server, {
-        cors: {
-            origin: '*',
-            methods: ['GET', 'POST'],
-        },
-    })
+	const io = new Server(server, {
+		cors: {
+			origin: '*',
+			methods: ['GET', 'POST'],
+		},
+	})
 
-    // Setup socket server
-    io.use(AuthHandler)
-    io.on('connection', (socket: Socket) => {
-        iotHandler(socket, io)
-    })
+	// Setup socket server
+	io.use(AuthHandler)
+	io.on('connection', (socket: Socket) => {
+		iotHandler(socket, io)
+	})
 
-    // Start MQTT when Socket starts
-    initMqtt(io)
+	// Start MQTT when Socket starts
+	initMqtt(io)
 }
 
 export default socketInit
