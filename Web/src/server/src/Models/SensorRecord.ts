0454ddf8c768a916e27b20605c6c4e3e4c47699f
Livens <dangkhoah77@gmail.com>
2025-12-20 14:05:10 +0700

    [refactor] correct interface name from IUser to ISensorRecord in SensorRecord.ts

--- Web/src/server/src/Models/SensorRecord.ts
+++ Web/src/server/src/Models/SensorRecord.ts
@@ -5,7 +5,7 @@ import { SensorRecordType } from 'Shared/Data/Types/index.js'
 /**
  * Interface representing a Sensor document in MongoDB.
  *
- * @interface IUser
+ * @interface ISensorRecord
  * @property {SensorData} data - The sensor data
  * @property {Date} timestamp - The timestamp of the record
  */
@@ -15,15 +15,15 @@ export interface ISensorRecord extends SensorRecordType, Document {}
  * Mongoose schema for the Sensor model.
  */
 const SensorRecordSchema = new Schema({
-    data: {
-        type: {
-            temperature: { type: Number, required: true },
-            humidity: { type: Number, required: true },
-            moisture: { type: Number, required: true },
-        },
-        required: true,
-    },
-    timestamp: { type: Date, required: true, unique: true },
+	data: {
+		type: {
+			temperature: { type: Number, required: true },
+			humidity: { type: Number, required: true },
+			moisture: { type: Number, required: true },
+		},
+		required: true,
+	},
+	timestamp: { type: Date, required: true, unique: true },
 })
 
 export default model('SensorRecord', SensorRecordSchema)
